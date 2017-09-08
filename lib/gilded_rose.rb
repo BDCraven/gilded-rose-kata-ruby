87b95637695eef5c36c97559be5855b768b03318
BDCraven <bencraven@hotmail.com>
2017-09-08 15:31:40 +0100

    Simplified sellin conditionals

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -56,26 +56,21 @@ end
       end
 
       if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+        item.sell_in -= 1
       end
 
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+      if regular_item?(item) && item.sell_in < 0
+        reduce_quality(item)
       end
+
+      if item.name == "Aged Brie" && item.sell_in < 0
+        increase_quality(item)
+      end
+
+      if backstage_pass?(item) && item.sell_in < 0
+        item.quality = item.quality - item.quality
+      end
+
     end
   end
 end
